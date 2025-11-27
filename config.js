ac11660ebe230ea971c62acc892a33439185770e
ahmad <at2951090@gmail.com>
2025-11-27 22:48:55 +0300

    12

---
+++ config.js
@@ -0,0 +1,4 @@
+// املأ القيم التالية من مشروع Supabase الخاص بك
+// لا تنشر المفاتيح الحساسة علنًا. هذا الملف للاستخدام التجريبي أو ضمن بيئة موثوقة.
+window.SUPABASE_URL = "https://fwqickaaqqcynpzuwdso.supabase.co"; // مثال: https://xyzcompany.supabase.co
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3cWlja2FhcXFjeW5wenV3ZHNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNjk5NTAsImV4cCI6MjA3OTY0NTk1MH0.VdZoBCuiUzjGV3qoZ856nBkrctyDSFkx-5RLLAyDBoc"; // المفتاح العام (anon)
